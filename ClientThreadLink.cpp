f1e7faf5ed6b53865a9497e472a788a5891906ce
X512 <danger_mail@list.ru>
2023-01-25 03:12:45 +0900

    add connection object, fix crash on add-on unload

--- ClientThreadLink.cpp
+++ ClientThreadLink.cpp
@@ -1,34 +1,21 @@
 #include "ClientThreadLink.h"
+#include <PthreadMutexLocker.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <new>
 
 
-thread_local ObjectDeleter<ClientThreadLink> tlsClientThreadLink = NULL;
-
-
-ClientThreadLink::ClientThreadLink(port_id serverPort):
-	fPort(create_port(100, "client"))
-{
-	fprintf(stderr, "+ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
-	fLink.SetTo(serverPort, fPort.Get());
-	int32 replyCode;
-	port_id serverThreadPort;
-	fLink.StartMessage(connectMsg);
-	fLink.Attach<int32>(fPort.Get());
-	fLink.FlushWithReply(replyCode);
-	fLink.Read<int32>(&serverThreadPort);
-	fLink.SetTo(serverThreadPort, fPort.Get());
-}
-
-ClientThreadLink::ClientThreadLink(const BMessenger &serverMsgr):
-	fPort(create_port(100, "client"))
+ClientThreadLink::ClientThreadLink(ClientThreadLinkConnection *conn, const BMessenger &serverMsgr):
+	fConn(conn), fPort(create_port(100, "client"))
 {
 	fprintf(stderr, "+ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
 	fLink.SetTo(-1, fPort.Get());
 	int32 replyCode;
 	port_id serverThreadPort;
 	BMessage msg(connectMsg);
-	msg.AddInt32("port", fPort.Get()); 
+	msg.AddInt32("port", fPort.Get());
 	serverMsgr.SendMessage(&msg);
 	fLink.GetNextMessage(replyCode);
 	fLink.Read<int32>(&serverThreadPort);
@@ -40,23 +27,40 @@ ClientThreadLink::~ClientThreadLink()
 	fprintf(stderr, "-ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
 	fLink.StartMessage(disconnectMsg);
 	fLink.Flush();
+	PthreadMutexLocker lock(&fConn->fLock);
+	fConn->fLinks.Remove(this);
 }
 
 
-ClientThreadLink *GetClientThreadLink(port_id serverPort)
+ClientThreadLinkConnection::ClientThreadLinkConnection()
 {
-	if (!tlsClientThreadLink.IsSet()) {
-		ClientThreadLink *threadLink = new ClientThreadLink(serverPort);
-		tlsClientThreadLink.SetTo(threadLink);
-	}
-	return tlsClientThreadLink.Get();
+	if (pthread_key_create(&fLinkTls, [](void *arg) {
+		delete static_cast<ClientThreadLink*>(arg);
+	}) < 0) abort();
 }
 
-ClientThreadLink *GetClientThreadLink(const BMessenger &serverMsgr)
+ClientThreadLinkConnection::~ClientThreadLinkConnection()
 {
-	if (!tlsClientThreadLink.IsSet()) {
-		ClientThreadLink *threadLink = new ClientThreadLink(serverMsgr);
-		tlsClientThreadLink.SetTo(threadLink);
+	PthreadMutexLocker lock(&fLock);
+	while (ClientThreadLink *threadLink = fLinks.First()) {
+		delete threadLink;
 	}
-	return tlsClientThreadLink.Get();
+	if (pthread_key_delete(fLinkTls) < 0) abort();
+}
+
+void ClientThreadLinkConnection::SetMessenger(const BMessenger &serverMsgr)
+{
+	fServerMsgr = serverMsgr;
+}
+
+ClientThreadLink *ClientThreadLinkConnection::GetLink()
+{
+	ClientThreadLink *threadLink = (ClientThreadLink*)pthread_getspecific(fLinkTls);
+	if (threadLink != NULL) return threadLink;
+	PthreadMutexLocker lock(&fLock);
+	threadLink = new(std::nothrow) ClientThreadLink(this, fServerMsgr);
+	if (threadLink == NULL) return NULL;
+	fLinks.Insert(threadLink);
+	pthread_setspecific(fLinkTls, threadLink);
+	return threadLink;
 }
--- ClientThreadLink.h
+++ ClientThreadLink.h
@@ -3,6 +3,8 @@
 #include <PortLink.h>
 #include <AutoDeleterOS.h>
 #include <Messenger.h>
+#include <pthread.h>
+#include <DoublyLinkedList.h>
 
 
 enum {
@@ -13,19 +15,42 @@ enum {
 };
 
 
+class ClientThreadLinkConnection;
+
 class ClientThreadLink {
 private:
+	ClientThreadLinkConnection *fConn;
 	BPrivate::PortLink fLink;
 	PortDeleter fPort;
+	DoublyLinkedListLink<ClientThreadLink> fListLink;
 
 public:
-	ClientThreadLink(port_id serverPort);
-	ClientThreadLink(const BMessenger &serverMsgr);
+	typedef DoublyLinkedList<
+		ClientThreadLink, DoublyLinkedListMemberGetLink<ClientThreadLink, &ClientThreadLink::fListLink>
+	> List;
+
+public:
+	ClientThreadLink(ClientThreadLinkConnection *conn, const BMessenger &serverMsgr);
 	~ClientThreadLink();
 
 	inline BPrivate::PortLink &Link() {return fLink;}
 };
 
+class ClientThreadLinkConnection {
+private:
+	friend class ClientThreadLink;
 
-ClientThreadLink *GetClientThreadLink(port_id serverPort);
-ClientThreadLink *GetClientThreadLink(const BMessenger &serverMsgr);
+	pthread_mutex_t fLock = PTHREAD_MUTEX_INITIALIZER;
+	BMessenger fServerMsgr;
+	pthread_key_t fLinkTls;
+	ClientThreadLink::List fLinks;
+
+public:
+	ClientThreadLinkConnection();
+	~ClientThreadLinkConnection();
+	const BMessenger &Messenger() const {return fServerMsgr;}
+	void SetMessenger(const BMessenger &serverMsgr);
+
+	ClientThreadLink *GetLink();
+};
+
